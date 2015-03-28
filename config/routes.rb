4778af64fa65e3016817006e30f776d730fdf0c6
bwoodfork <byronwoodfork@gmail.com>
2015-03-28 15:27:15 -0500

    Added user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/help', as: 'help'
-  get 'static_pages/about', as: 'about'
-  get 'static_pages/contact', as: 'contact'
-  get 'users/new', as: 'signup'
+  get 'static_pages/help', to: 'static_pages#help', as: :help
+  get 'static_pages/about', to: 'static_pages#about', as: :about
+  get 'static_pages/contact', to: 'static_pages#contact', as: :contact
+  get 'signup', to: 'users#new', as: :signup
+  resources :users
 end
